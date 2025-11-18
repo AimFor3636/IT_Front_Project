ee93382cd78da7e161c986dd413182794d298745
sangwoo <okqwaszx12@naver.com>
2025-11-19 05:17:58 +0900

    로그아웃 관련 사전 처리

--- 소스코드/commonScriptDepth.js
+++ 소스코드/commonScriptDepth.js
@@ -205,6 +205,11 @@
         document.querySelectorAll(".navbar-nav .nav-link").forEach(l => l.classList.remove("active"));
       });
     });
+
+      // 로그아웃
+    document.getElementById("logout").addEventListener("click", () => {
+      console.log("온다.");
+    })  
   };
 
   const updateBreadcrumb = (parentMenu, currentMenu) => {
